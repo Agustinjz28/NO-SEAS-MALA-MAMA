732fd3add155ec9d528f2e429d2dff90b53aa67e
Agustinjz28 <139351124+Agustinjz28@users.noreply.github.com>
2023-07-12 11:15:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('estan en lo correcto , piensan lo mismo que yo jaja')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function () {
+    alert('ya se que pusieron no para no ser malos , pero tengo razon jaja') 
+})
